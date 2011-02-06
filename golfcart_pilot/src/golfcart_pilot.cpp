ae5f78fefe172d529b92afec234439aa19e0ae8d
Adam Miller <kirov.sama@gmail.com>
2011-02-06 14:28:38 -0800

    Add outline for golfcart_common and golfcart_pilot. include steering constants in golfcart.h

---
+++ golfcart_pilot/src/golfcart_pilot.cpp
@@ -0,0 +1,8 @@
+#include <ros/ros.h>
+#include "golfcart_common/golfcart.h"
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "golfcart_pilot");
+    ros::NodeHandle n;
+    return 0;
+}
