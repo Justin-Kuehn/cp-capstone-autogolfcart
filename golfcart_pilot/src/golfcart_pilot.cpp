8b2c22dd8229c0c6003723d16570f45ec7f82c70
Adam Miller <kirov.sama@gmail.com>
2011-02-06 14:35:07 -0800

    golfcart_pilot usage printing

--- golfcart_pilot/src/golfcart_pilot.cpp
+++ golfcart_pilot/src/golfcart_pilot.cpp
@@ -1,8 +1,17 @@
+#include <iostream>
 #include <ros/ros.h>
 #include "golfcart_common/golfcart.h"
+
+using namespace std;
 
 int main(int argc, char **argv) {
     ros::init(argc, argv, "golfcart_pilot");
     ros::NodeHandle n;
+
+    if (argc != 4) {
+        cerr << "Usage: " << argv[0] << " <min> <center> <max>" << endl;
+        return 1;
+    }
+
     return 0;
 }
