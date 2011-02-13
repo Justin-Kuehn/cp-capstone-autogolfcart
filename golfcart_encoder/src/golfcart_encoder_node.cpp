da14c543853a1275506ac95648620c6f9e8ddfd9
Justin <justin@justin-laptop.(none)>
2011-02-13 15:11:45 -0800

    fixed encoder node to actually work

--- golfcart_encoder/src/golfcart_encoder_node.cpp
+++ golfcart_encoder/src/golfcart_encoder_node.cpp
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <math.h>
 #include <unistd.h>
 #include <limits.h>
@@ -12,26 +13,35 @@ using std::string;
 static const double WHEEL_CIRCUM = 1.1; // Meters
 static const double TICKS_PER_REV = 160.0;
 static const int ARRAY_SIZE = 20;
+static const int MAX_ENCODER_TICKS = 32000;
 
 int main(int argc, char **argv)
 {
   int last_tick = 0, delta_tick = 0, rolling_index = 0;
-  ros::Time last_time;
-  ros::Duration delta_time;
   double speed[ARRAY_SIZE] = {0};
   double avg_speed = 0;
+  ros::Time last_time;
+  ros::Duration delta_time;
+  
+  SimpleSerial* port;
+  
   
   ros::init(argc, argv, "golfcart_encoder");
   ros::NodeHandle nh;
   
-  SimpleSerial port("/dev/ttyUSB0", 4800);
+  try {
+    port = new SimpleSerial("/dev/ttyUSB0", 9600);
+  } catch (...) {
+    fprintf(stderr, "golfcart_encoder: Error trying to open serial port\n");
+    return -1;
+  }
   
   ros::Publisher pb = 
     nh.advertise<golfcart_encoder::GolfcartEncoder>("encoder", 1);
   
-  ros::Rate loop_rate(10); // 10Hz
+  ros::Rate loop_rate(100); // 100Hz
   
-  last_tick = atoi(port.readLine().c_str());
+  last_tick = atoi(port->readLine().c_str());
   last_time = ros::Time::now();
   
   while(ros::ok())
@@ -39,20 +49,22 @@ int main(int argc, char **argv)
     golfcart_encoder::GolfcartEncoder msg;
     
     // Read from encoders
-    msg.ticks = atoi(port.readLine().c_str());
+    msg.ticks = atoi(port->readLine().c_str());
     msg.header.stamp = ros::Time::now();
     msg.heading = 0; //TODO
     
     // Calculate Speed
     delta_time = msg.header.stamp - last_time;
     delta_tick = msg.ticks - last_tick;
-    if (delta_tick < 0) delta_tick += INT_MAX;
+    if (delta_tick < 0) delta_tick += MAX_ENCODER_TICKS;
     speed[rolling_index] = 
       (WHEEL_CIRCUM / TICKS_PER_REV) * (delta_tick / delta_time.toSec());
     rolling_index = (rolling_index + 1) % ARRAY_SIZE;
     last_time = msg.header.stamp;
+    last_tick = msg.ticks;
     
     // Calculate Average Speed
+    avg_speed = 0;
     for(int i = 0; i < ARRAY_SIZE; i++) 
     {
       avg_speed += speed[i];
