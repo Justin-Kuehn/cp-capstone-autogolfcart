b0c468248547df547e60c812c5ac738b70290de4
Justin <justin@justin-laptop.(none)>
2011-02-13 18:09:13 -0800

    Added localization pacakage.

---
+++ golfcart_localization/src/particle_filter.cpp
@@ -0,0 +1,11 @@
+#include "golfcart_localization/particle_filter.h" 
+
+void ParticleFilter::Init(double startLat, double startLon, double startHeading, int max_ticks) {
+
+}
+
+/*
+void ParticleFilter :: RunIteration(int odomTicks, double lat, double lon, double heading, double deltaTime); {
+
+}
+*/
