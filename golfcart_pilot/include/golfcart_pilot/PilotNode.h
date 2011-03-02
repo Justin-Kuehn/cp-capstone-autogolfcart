65bb5204ddd7ecfe594671d1305430021c1e40d6
David Allender <dsalvado@calpoly.edu>
2011-03-02 11:45:21 -0800

    Added speed to pilot node

--- golfcart_pilot/include/golfcart_pilot/PilotNode.h
+++ golfcart_pilot/include/golfcart_pilot/PilotNode.h
@@ -5,6 +5,7 @@
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
 #include "roboteq_ax1500/channel_forward.h"
+#include "pmad/Switch.h"
 
 struct SteeringParams {
     int chan;
@@ -18,15 +19,22 @@ struct SteeringParams {
     double range;
 };
 
+struct SpeedParams {
+    int min;
+    int max;
+    double ticPerPercent;
+};
+
 class PilotNode {
     ros::NodeHandle n;
     ros::NodeHandle pn;
     ros::ServiceClient ax1500;
+    ros::ServiceClient arduino;
     ros::Subscriber absCmdSub;
     ros::Subscriber relCmdSub;
     geometry_msgs::Twist state;
-    SteeringParams sParams;
-
+    SteeringParams stParams;
+    SpeedParams spParams;
 
     public:
     PilotNode() : pn("~") {};
@@ -35,6 +43,7 @@ class PilotNode {
     void absoluteCommandCallback(const geometry_msgs::Twist::ConstPtr &m);
     void relativeCommandCallback(const geometry_msgs::Twist::ConstPtr &m);
     bool sendSteeringCommand(uint8_t value);
+    bool sendSpeedCommand(uint8_t value);
 };
 
 #endif
