7356e0be84b37d588aa1346c93ec5939d7f35be0
Justin <justin@justin-laptop.(none)>
2011-03-03 20:44:13 -0800

    sync'd gui bridge with GUI to recieve simple commands

--- gui_bridge/nodes/gui_bridge_node.py
+++ gui_bridge/nodes/gui_bridge_node.py
@@ -2,28 +2,38 @@
 import roslib; roslib.load_manifest('gui_bridge')
 import rospy
 import socket
-from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Twist, Vector3
 from golfcart_encoder.msg import GolfcartEncoder
 
 def gui_bridge():	
 	#setup ROS node
 	rospy.init_node('gui_bridge')
 	pub = rospy.Publisher('cmd_vel', Twist)
+	
 	#setup socket
 	HOST = ''
-	PORT = 50007
+	PORT = 50011
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	s.bind((HOST, PORT))
 	print 'Waiting for connection on port ', PORT
 	s.listen(1)
 	conn, addr = s.accept()
 	print 'Connected by', addr
+	
+	rospy.sleep(0.5)	
+	
 	while not rospy.is_shutdown():
-		guiCmd = eval(s.recv(1024))
-		#do whatever with data
-		# ('[0-100],0,0' '0,[ -0.7rads, +0.7rads ], 0) 
-		pub.publish('0,0,0', '0,0,0')
-		rospy.sleep(0.1) # 10Hz
+		guiCmd = eval(conn.recv(1024))
+		print guiCmd
+		wheel_angle = float(guiCmd['wheel']) * 0.0174532925 # radians
+		speed = float(guiCmd['speed'])
+		
+		command = Twist()
+		command.linear = Vector3 (wheel_angle,0,0)
+		command.angular = Vector3 (0,speed,0)
+		pub.publish(command)
+		rospy.sleep(0.2) # 5Hz
+
 if __name__ == '__main__':
 	try:
 		gui_bridge()
