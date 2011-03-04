4e01e390d7f7e8be9a2c1463e7b0d56fc9740211
Justin <justin@justin-laptop.(none)>
2011-03-03 21:23:06 -0800

    fixed some bugs and added some safety checks to gui_bridge

--- gui_bridge/nodes/gui_bridge_node.py
+++ gui_bridge/nodes/gui_bridge_node.py
@@ -12,7 +12,7 @@ def gui_bridge():
 	
 	#setup socket
 	HOST = ''
-	PORT = 50011
+	PORT = 50012
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	s.bind((HOST, PORT))
 	print 'Waiting for connection on port ', PORT
@@ -23,16 +23,30 @@ def gui_bridge():
 	rospy.sleep(0.5)	
 	
 	while not rospy.is_shutdown():
-		guiCmd = eval(conn.recv(1024))
-		print guiCmd
-		wheel_angle = float(guiCmd['wheel']) * 0.0174532925 # radians
-		speed = float(guiCmd['speed'])
+		try:
+			data = conn.recv(1024)
+			if data.count("{") > 1:
+				data = data.split("}{")
+				data = "{"+data[len(data)-1]
+			guiCmd = eval(data)
+			print guiCmd
+			wheel_angle = float(guiCmd['wheel']) * 0.0174532925 # d2r
+			speed = float(guiCmd['speed'])
 		
-		command = Twist()
-		command.linear = Vector3 (wheel_angle,0,0)
-		command.angular = Vector3 (0,speed,0)
-		pub.publish(command)
-		rospy.sleep(0.2) # 5Hz
+			command = Twist()
+			command.linear = Vector3 (speed,0,0)
+			command.angular = Vector3 (0,wheel_angle,0)
+			pub.publish(command)
+			rospy.sleep(0.2) # 5Hz
+		except socket.error:
+			command = Twist()
+			command.linear = Vector3 (0,0,0)
+			command.angular = Vector3 (0,0,0)
+			pub.publish(command)
+			print 'gui_bridge: SOCKET ERROR OCCURED, STOPPING DRIVE OPERATIONS'
+			break
+			
+	s.close()
 
 if __name__ == '__main__':
 	try:
