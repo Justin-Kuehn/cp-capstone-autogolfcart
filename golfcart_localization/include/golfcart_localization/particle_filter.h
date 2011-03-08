d1f96371e6d399d83a692ce079cf3fea5e42d6f3
Justin <justin@justin-laptop.(none)>
2011-03-08 15:18:15 -0800

    Particle filter updates

--- golfcart_localization/include/golfcart_localization/particle_filter.h
+++ golfcart_localization/include/golfcart_localization/particle_filter.h
@@ -23,18 +23,24 @@ typedef struct PARTICLE_t {
 } Particle;
 
 class ParticleFilter {
+  static const double D2R = 0.0174532925; // Degrees to Rad
+  static const double R2D = 57.2957795;   // Radians to Deg
   static const int NUM_PARTICLES = 100;
   static const int startX = 0;
   static const int startY = 0;
   
-  Particle CurrentState;
   vector<Particle> ParticleList;
-  vector<Particle> ReprojList; 
+  vector<Particle> PropogatedList; 
   
   double startLat;
   double startLon;
   double startHeading;
   
+  double currX;
+  double currY;
+  double currHeading;
+  
+  double highestWeight; // Used for normalizing particle weights
   int lastTick;
   OdomParam odomModel;
 
@@ -62,15 +68,18 @@ public:
       double heading,     // deg
       double deltaTime    // sec
       );
+   
+private:
+
   /*
    * Propogates the src particle using the odometry model and 
    * stores the result in dst.
    */
-  void PropogateParticle(
+  void PropogateParticles(
       int odomTicks,      // encoder ticks
-      double heading,     // deg
-      Particle &src,      // Particle Source
-      Particle &dest      // Particle Destination
+      double lat, 
+      double lon,
+      double heading     // deg
       );
   /*
    * Assigns a weight to the particle based on the given gps coordinates.
@@ -84,7 +93,17 @@ public:
    * Resamples all particles based on each particles weight
    */
   void ResampleParticles();
+ 
+ /*
+  * Randomly samples a zero centered normal distribution from a with
+  * variance var.
+  */
+  double sample_normal_dist(double var);
   
+  /*
+   * Adds two angles together correcting for rollover in degrees
+   */
+  double addAngle(double a1, double a2);
     
 };
 
--- golfcart_localization/src/particle_filter.cpp
+++ golfcart_localization/src/particle_filter.cpp
@@ -1,3 +1,6 @@
+#include <ctime> 
+#include <cstdlib> 
+#include <math.h>
 #include "golfcart_localization/particle_filter.h" 
 
 void ParticleFilter::Init(double startLat, double startLon, double startHeading, int odomTicks, OdomParam odomModel) {
@@ -6,10 +9,10 @@ void ParticleFilter::Init(double startLat, double startLon, double startHeading,
   this->startHeading = startHeading;
   this->lastTick = odomTicks;
   this->odomModel = odomModel;
-  
+    
   // Zero All Particles
   ParticleList.resize(NUM_PARTICLES);
-  ReprojList.resize(NUM_PARTICLES);
+  PropogatedList.resize(NUM_PARTICLES);
   
   for(unsigned int i = 0; i < ParticleList.size(); i++) {
     ParticleList[i].X = startX;
@@ -17,29 +20,74 @@ void ParticleFilter::Init(double startLat, double startLon, double startHeading,
     ParticleList[i].Heading = startHeading;
   }
   
-  CurrentState.X = startX;
-  CurrentState.Y = startY;
-  CurrentState.Heading = startHeading;
+  currX = startX;
+  currY = startY;
+  currHeading = startHeading;
+  
+  srand(time(0));  // Initialize random number generator.
 }
 
 void ParticleFilter::RunIteration(int odomTicks, double lat, double lon, double heading, double deltaTime) {
+  if(odomTicks == lastTick) return; // Do nothing if we have not moved
   
-  // Propogate Particles
+  PropogateParticles(odomTicks, lat, lon, heading);
+  
+  ResampleParticles();
+
+  // Calculated Average
+  double x_total = 0;
+  double y_total = 0;
+  double tx_total = 0;
+  double ty_total = 0;
+
   for(unsigned int i = 0; i < ParticleList.size(); i++) {
-    PropogateParticle(odomTicks, heading, ParticleList[i]);
+  	x_total += ParticleList[i].X;
+  	y_total += ParticleList[i].Y;
+  	tx_total += cos(heading * D2R);
+  	ty_total += sin(heading * D2R);
   }
   
-  // Weight Particles
-  
-  // Resample Particles
-  
-  // Calculated Average
-  
-  
+  this->currX = x_total;
+  this->currY = y_total;
+  this->currHeading = R2D * atan2(ty_total, tx_total);
 }
 
-void ParticleFilter::PropogateParticle(int odomTicks, double heading, Particle &src) {
+void ParticleFilter :: ResampleParticles() {
+  if(highestWeight == 0) { ParticleList = PropogatedList; return; }
   
+  vector<Particle> sampleSpace (NUM_PARTICLES);
+  
+	for(unsigned int i = 0; i < PropogatedList.size(); i++) {
+	  PropogatedList[i].Weight = PropogatedList[i].Weight / highestWeight;
+	  
+	  if(PropogatedList[i].Weight > 0.8) {
+	    for(int j = 0; j < 5; j++)
+	      sampleSpace.push_back(PropogatedList[i]);
+	  }
+	  else if(PropogatedList[i].Weight > 0.7) {
+	    for(int j = 0; j < 4; j++)
+	      sampleSpace.push_back(PropogatedList[i]);
+	  }
+	  else if(PropogatedList[i].Weight > 0.6) {
+	    for(int j = 0; j < 3; j++)
+	      sampleSpace.push_back(PropogatedList[i]);
+	  }
+	  else if(PropogatedList[i].Weight > 0.5) {
+	    for(int j = 0; j < 2; j++)
+	      sampleSpace.push_back(PropogatedList[i]);
+	  }
+	  else if(PropogatedList[i].Weight > 0.4) {
+	    for(int j = 0; j < 1; j++)
+	      sampleSpace.push_back(PropogatedList[i]);
+	  }
+	}
+	
+	for(unsigned int i = 0; i < ParticleList.size(); i++) {
+	  ParticleList[i] = sampleSpace[rand() % sampleSpace.size()];
+	}
+}
+
+void ParticleFilter::PropogateParticles(int odomTicks, double lat, double lon, double heading) {
   int deltaTick = odomTicks - lastTick;
   
   // Check for encoder rollover
@@ -47,7 +95,52 @@ void ParticleFilter::PropogateParticle(int odomTicks, double heading, Particle &
     deltaTick += odomModel.maxTicks;
   }
   
+  double distTravelled = deltaTick * odomModel.wheelCircum / odomModel.ticksPerRev;
+  double angleTraveled;
   
+  // Propogate each particle
+  highestWeight = 0;  
+  for(unsigned int i = 0; i < ParticleList.size(); i++) {
+  	angleTraveled = heading - ParticleList[i].Heading;
+  	
+    // Add randomness
+    double trans = distTravelled - sample_normal_dist(0.1 * angleTraveled + 0.5 * distTravelled);
+    double rot   = angleTraveled - sample_normal_dist(0.5 * angleTraveled + 0.1 * distTravelled);
     
+    PropogatedList[i].X = ParticleList[i].X + trans * cos(D2R * ParticleList[i].Heading + D2R * angleTraveled);
+    PropogatedList[i].Y = ParticleList[i].Y + trans * sin(D2R * ParticleList[i].Heading + D2R * angleTraveled);;
+    PropogatedList[i].Heading = addAngle(ParticleList[i].Heading, rot);
+    
+    WeightParticle (lat, lon, PropogatedList[i] );
+    
+    if(highestWeight < PropogatedList[i].Weight) 
+      highestWeight = PropogatedList[i].Weight;
+  }
+}
+
+void ParticleFilter::WeightParticle(double lat, double lon, Particle &src) {
+	src.Weight = 0;
+}
+
+/*
+* Adds two angles together correcting for rollover in degrees
+*/
+double ParticleFilter :: addAngle( double ang1, double ang2 ) {
+  double sum = ang1 + ang2;
+  while (sum > 360) sum -= 360;
+  while (sum < 0 )	sum += 360;
+  return sum;
+}
+/*
+ * Randomly samples a zero centered normal distribution from a with
+ * variance 'var'.
+ */
+double ParticleFilter :: sample_normal_dist(double var) {
+  double sum = 0;
+  double stddev = sqrt ( var );
+  for ( int i = 0; i < 12; ++i ) {
+    sum += (rand() / (static_cast<double>(RAND_MAX) + 1.0)) * (2 * stddev) - stddev;
+  }
+  return sum;
 }
       
